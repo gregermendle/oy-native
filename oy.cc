ec7771e1b0154160a1de3c6631cc24af84785658
Greg <gjordan@umassd.edu>
2018-12-06 23:57:08 -0500

    Use proper memery

--- oy.cc
+++ oy.cc
@@ -1,7 +1,7 @@
 #include <nan.h>
 
 void Method(const Nan::FunctionCallbackInfo<v8::Value>& info) {
-  info.GetReturnValue().Set(Nan::New("You wot bruv?!").ToLocalChecked());
+  info.GetReturnValue().Set(Nan::New("You wot m8?!").ToLocalChecked());
 }
 
 void Init(v8::Local<v8::Object> exports, v8::Local<v8::Object> module) {
